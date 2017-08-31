7315e0aa2cc65dc69277595b7433d1c7bffc18cc
jmcordier <jmcordier@laprovence.com>
2017-08-31 19:20:19 +0200

    page 61

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  HostBinding, 
+  Input 
+} from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -8,22 +13,17 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  @Input() article: Article;
 
   constructor() { 
-    this.article = new Article(
-      'Angular 2',
-      'http://angular.io',
-      10
-    );
   }
 
-  voteUp() {
+  voteUp(): boolean {
     this.article.voteUp();
     return false;
   }
 
-  voteDown() {
+  voteDown(): boolean {
     this.article.voteDown();
     return false;
   }
